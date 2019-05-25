b527fa579667248cc23261c285e083e8f32d1a83
jolil <jolil@kth.se>
2019-05-25 10:28:57 +0200

    Site done

---
+++ precache-manifest.3f867066c6a09d6c5085fc39b3979c88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67ada68d31ee9d32b93c",
+    "url": "/Stwitch/static/css/main.075320ab.chunk.css"
+  },
+  {
+    "revision": "67ada68d31ee9d32b93c",
+    "url": "/Stwitch/static/js/main.67ada68d.chunk.js"
+  },
+  {
+    "revision": "d0e02aa03df3c0c1c9a1",
+    "url": "/Stwitch/static/js/runtime~main.d0e02aa0.js"
+  },
+  {
+    "revision": "c1537c055fc2934129a5",
+    "url": "/Stwitch/static/js/2.c1537c05.chunk.js"
+  },
+  {
+    "revision": "951fa131640c46724d24e2909316ffcb",
+    "url": "/Stwitch/index.html"
+  }
+];
